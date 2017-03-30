cf601e00e117e44300df6ced2269bbe68795cdfa
Cobalt <toutcobalt@gmail.com>
2017-03-30 13:05:29 +0200

    Feels that better like this; also add support form html files

--- gulpfile.js
+++ gulpfile.js
@@ -8,29 +8,39 @@ const fs = require('fs'),
     runSequence = require('run-sequence'),
     del = require('del');
 
-gulp.task('bundle', ['js'], function() {
+gulp.task('bundle', function() {
     return browserify('./server/client/index.js')
-        .transform(babelify, {
-            presets: ['es2015'],
-            plugins: ['transform-runtime']
-        })
-        .bundle()
-        .pipe(fs.createWriteStream('./server/public/bundle.js'));
+    .transform(babelify, {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    })
+    .bundle()
+    .pipe(fs.createWriteStream('./server/public/bundle.js'));
 });
 
-gulp.task('js', ['vueify'], function() {
+gulp.task('editlinks', function() {
+    return gulp.src('./server/client/**/*.js')
+    .pipe(replace(/(require\('.*)\.vue'\)/g, '$1.js\')'))
+    .pipe(gulp.dest('./server/client/'));
+});
+
+gulp.task('js', function() {
     return gulp.src('./client/**/*.js')
-        .pipe(replace(/(require\('.*)\.vue'\)/g, '$1.js\')'))
-        .pipe(gulp.dest('./server/client/'));
+    .pipe(gulp.dest('./server/client/'));
 });
 
-gulp.task('vueify', ['clean'], function() {
+gulp.task('html', function() {
+    return gulp.src('./client/**/*.html')
+    .pipe(gulp.dest('./server/client/'));
+});
+
+gulp.task('vue', function() {
     return gulp.src('./client/**/*.vue')
-        .pipe(gulpVueify({
-            extractCSS: true,
-            CSSOut: './server/public/bundle.css'
-        }))
-        .pipe(gulp.dest('./server/client/'));
+    .pipe(gulpVueify({
+        extractCSS: true,
+        CSSOut: './server/public/bundle.css'
+    }))
+    .pipe(gulp.dest('./server/client/'));
 });
 
 gulp.task('clean', function() {
@@ -38,9 +48,11 @@ gulp.task('clean', function() {
 });
 
 gulp.task('watch', function() {
-    return watch(['./client/**/*.js', './client/**/*.vue', './client/**/*.less'], function() {
-        runSequence('bundle');
+    return watch(['./client/**/*.js', './client/**/*.vue', './client/**/*.less', './client/**/*.html'], {
+        ignoreInitial: false
+    }, function() {
+        runSequence('clean', ['vue', 'html', 'js'], 'editlinks', 'bundle');
     });
 });
 
-gulp.task('default', ['bundle', 'watch']);
+gulp.task('default', ['watch']);
